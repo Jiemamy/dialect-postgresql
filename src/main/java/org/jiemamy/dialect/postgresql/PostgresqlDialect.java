880baaf6dbbda4e00975acdc42f0a9eed7f608f7
MIYAMOTO Daisuke <dai.0304@gmail.com>
2011-01-21 02:11:25 +0000

    [CORE-0] Dialectのインターフェイスと骨格実装整備。

--- src/main/java/org/jiemamy/dialect/postgresql/PostgresqlDialect.java
+++ src/main/java/org/jiemamy/dialect/postgresql/PostgresqlDialect.java
@@ -34,20 +34,16 @@ import static org.jiemamy.model.datatype.DataTypeCategory.TIMESTAMP;
 import static org.jiemamy.model.datatype.DataTypeCategory.VARBIT;
 import static org.jiemamy.model.datatype.DataTypeCategory.VARCHAR;
 
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 
 import org.jiemamy.dialect.AbstractDialect;
 import org.jiemamy.dialect.TypeParameterSpec;
 import org.jiemamy.dialect.TypeParameterSpec.Necessity;
 import org.jiemamy.model.datatype.DefaultTypeReference;
 import org.jiemamy.model.datatype.TypeParameterKey;
-import org.jiemamy.model.datatype.TypeReference;
 import org.jiemamy.validator.CompositeValidator;
 import org.jiemamy.validator.Validator;
 
@@ -58,65 +54,65 @@ import org.jiemamy.validator.Validator;
  */
 public class PostgresqlDialect extends AbstractDialect {
 	
-	private static Map<TypeReference, TypeParameterSpec[]> types;
+	private static List<Entry> typeEntries = Lists.newArrayList();
 	
 	static {
-		Map<TypeReference, TypeParameterSpec[]> map = Maps.newLinkedHashMap();
-		map.put(new DefaultTypeReference(INTEGER, "INTEGER", "int4"), new TypeParameterSpec[] {
-			new TypeParameterSpec(TypeParameterKey.SERIAL, Necessity.OPTIONAL),
-		});
-		map.put(new DefaultTypeReference(INTEGER, "BIGINT", "int8"), new TypeParameterSpec[] {
-			new TypeParameterSpec(TypeParameterKey.SERIAL, Necessity.OPTIONAL),
-		});
-		map.put(new DefaultTypeReference(SMALLINT), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(NUMERIC), new TypeParameterSpec[] {
+		// FORMAT-OFF
+		typeEntries.add(new Entry(new DefaultTypeReference(INTEGER, "INTEGER", "int4"), Arrays.asList(
+			new TypeParameterSpec(TypeParameterKey.SERIAL, Necessity.OPTIONAL)
+		)));
+		typeEntries.add(new Entry(new DefaultTypeReference(INTEGER, "BIGINT", "int8"), Arrays.asList(
+			new TypeParameterSpec(TypeParameterKey.SERIAL, Necessity.OPTIONAL)
+		)));
+		typeEntries.add(new Entry(new DefaultTypeReference(SMALLINT)));
+		typeEntries.add(new Entry(new DefaultTypeReference(NUMERIC), Arrays.asList(
 			new TypeParameterSpec(TypeParameterKey.PRECISION, Necessity.REQUIRED),
 			new TypeParameterSpec(TypeParameterKey.SCALE, Necessity.REQUIRED)
-		});
-		map.put(new DefaultTypeReference(DECIMAL), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(FLOAT, "REAL"), new TypeParameterSpec[] {
+		)));
+		typeEntries.add(new Entry(new DefaultTypeReference(DECIMAL)));
+		typeEntries.add(new Entry(new DefaultTypeReference(FLOAT, "REAL"), Arrays.asList(
 			new TypeParameterSpec(TypeParameterKey.PRECISION, Necessity.REQUIRED),
 			new TypeParameterSpec(TypeParameterKey.SCALE, Necessity.REQUIRED)
-		});
-		map.put(new DefaultTypeReference(REAL), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(DOUBLE, "DOUBLE PRECISION"), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(BIT), new TypeParameterSpec[] {
-			new TypeParameterSpec(TypeParameterKey.SIZE, Necessity.REQUIRED),
-		});
-		map.put(new DefaultTypeReference(CHARACTER), new TypeParameterSpec[] {
-			new TypeParameterSpec(TypeParameterKey.SIZE, Necessity.REQUIRED),
-		});
-		map.put(new DefaultTypeReference(VARCHAR), new TypeParameterSpec[] {
-			new TypeParameterSpec(TypeParameterKey.SIZE, Necessity.REQUIRED),
-		});
-		map.put(new DefaultTypeReference(CLOB, "TEXT"), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(BLOB, "BYTEA"), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(VARBIT), new TypeParameterSpec[] {
-			new TypeParameterSpec(TypeParameterKey.SIZE, Necessity.REQUIRED),
-		});
-		map.put(new DefaultTypeReference(BOOLEAN), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(DATE), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(TIME), new TypeParameterSpec[] {
-			new TypeParameterSpec(TypeParameterKey.WITH_TIMEZONE, Necessity.REQUIRED),
-		});
-		map.put(new DefaultTypeReference(TIMESTAMP), new TypeParameterSpec[] {
-			new TypeParameterSpec(TypeParameterKey.WITH_TIMEZONE, Necessity.REQUIRED),
-		});
-		map.put(new DefaultTypeReference(INTERVAL), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(OTHER, "UUID"), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(OTHER, "MACADDR"), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(OTHER, "MONEY"), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(OTHER, "INET"), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(OTHER, "CIDR"), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(OTHER, "XML"), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(OTHER, "LINE"), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(OTHER, "LSEG"), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(OTHER, "CIRCLE"), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(OTHER, "BOX"), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(OTHER, "PATH"), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(OTHER, "POINT"), new TypeParameterSpec[0]);
-		map.put(new DefaultTypeReference(OTHER, "POLYGON"), new TypeParameterSpec[0]);
-		PostgresqlDialect.types = ImmutableMap.copyOf(map);
+		)));
+		typeEntries.add(new Entry(new DefaultTypeReference(REAL)));
+		typeEntries.add(new Entry(new DefaultTypeReference(DOUBLE, "DOUBLE PRECISION")));
+		typeEntries.add(new Entry(new DefaultTypeReference(BIT), Arrays.asList(
+			new TypeParameterSpec(TypeParameterKey.SIZE, Necessity.REQUIRED)
+		)));
+		typeEntries.add(new Entry(new DefaultTypeReference(CHARACTER), Arrays.asList(
+			new TypeParameterSpec(TypeParameterKey.SIZE, Necessity.REQUIRED)
+		)));
+		typeEntries.add(new Entry(new DefaultTypeReference(VARCHAR), Arrays.asList(
+			new TypeParameterSpec(TypeParameterKey.SIZE, Necessity.REQUIRED)
+		)));
+		typeEntries.add(new Entry(new DefaultTypeReference(CLOB, "TEXT")));
+		typeEntries.add(new Entry(new DefaultTypeReference(BLOB, "BYTEA")));
+		typeEntries.add(new Entry(new DefaultTypeReference(VARBIT), Arrays.asList(
+			new TypeParameterSpec(TypeParameterKey.SIZE, Necessity.REQUIRED)
+		)));
+		typeEntries.add(new Entry(new DefaultTypeReference(BOOLEAN)));
+		typeEntries.add(new Entry(new DefaultTypeReference(DATE)));
+		typeEntries.add(new Entry(new DefaultTypeReference(TIME), Arrays.asList(
+			new TypeParameterSpec(TypeParameterKey.WITH_TIMEZONE, Necessity.REQUIRED)
+		)));
+		typeEntries.add(new Entry(new DefaultTypeReference(TIMESTAMP), Arrays.asList(
+			new TypeParameterSpec(TypeParameterKey.WITH_TIMEZONE, Necessity.REQUIRED)
+		)));
+		typeEntries.add(new Entry(new DefaultTypeReference(INTERVAL)));
+		typeEntries.add(new Entry(new DefaultTypeReference(OTHER, "UUID")));
+		typeEntries.add(new Entry(new DefaultTypeReference(OTHER, "MACADDR")));
+		typeEntries.add(new Entry(new DefaultTypeReference(OTHER, "MONEY")));
+		typeEntries.add(new Entry(new DefaultTypeReference(OTHER, "INET")));
+		typeEntries.add(new Entry(new DefaultTypeReference(OTHER, "CIDR")));
+		typeEntries.add(new Entry(new DefaultTypeReference(OTHER, "XML")));
+		typeEntries.add(new Entry(new DefaultTypeReference(OTHER, "LINE")));
+		typeEntries.add(new Entry(new DefaultTypeReference(OTHER, "LSEG")));
+		typeEntries.add(new Entry(new DefaultTypeReference(OTHER, "CIRCLE")));
+		typeEntries.add(new Entry(new DefaultTypeReference(OTHER, "BOX")));
+		typeEntries.add(new Entry(new DefaultTypeReference(OTHER, "PATH")));
+		typeEntries.add(new Entry(new DefaultTypeReference(OTHER, "POINT")));
+		typeEntries.add(new Entry(new DefaultTypeReference(OTHER, "POLYGON")));
+		// FORMAT-ON
 	}
 	
 
@@ -124,19 +120,11 @@ public class PostgresqlDialect extends AbstractDialect {
 	 * インスタンスを生成する。
 	 */
 	public PostgresqlDialect() {
-		super("jdbc:postgresql://localhost:5432/");
-	}
-	
-	public List<TypeReference> getAllTypeReferences() {
-		return Lists.newArrayList(types.keySet());
+		super("jdbc:postgresql://localhost:5432/", typeEntries);
 	}
 	
 	public String getName() {
 		return "PostgreSQL 8.1";
-	}
-	
-	public Collection<TypeParameterSpec> getTypeParameterSpecs(TypeReference reference) {
-		return Lists.newArrayList(types.get(reference));
 	}
 	
 	@Override
