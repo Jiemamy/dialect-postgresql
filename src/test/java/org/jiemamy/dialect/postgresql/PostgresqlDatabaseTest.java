401cc4db1c096b862a17cc597ced6e0049d46054
MIYAMOTO Daisuke <dai.0304@gmail.com>
2011-01-29 02:26:45 +0000

    [DPOS-0] データベーステストを追加。

---
+++ src/test/java/org/jiemamy/dialect/postgresql/PostgresqlDatabaseTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2007-2011 Jiemamy Project and the Others.
+ * Created on 2011/01/29
+ *
+ * This file is part of Jiemamy.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.jiemamy.dialect.postgresql;
+
+import java.util.Properties;
+
+import org.junit.Test;
+
+import org.jiemamy.test.AbstractDatabaseTest;
+
+/**
+ * TODO for daisuke
+ * 
+ * @version $Id$
+ * @author daisuke
+ */
+public class PostgresqlDatabaseTest extends AbstractDatabaseTest {
+	
+	/**
+	 * TODO for daisuke
+	 * 
+	 * @throws Exception 例外が発生した場合
+	 */
+	@Test
+	public void testname() throws Exception {
+		Properties properties = System.getProperties();
+		properties.list(System.out);
+	}
+	
+}
